fa4afc8fef5c63bff7c4bbfd60545beabf4ea651
mabere <mabere@taltech.ee>
2024-03-09 21:48:30 +0200

    seats

--- src/main/java/com/example/back/seat/service/SeatService.java
+++ src/main/java/com/example/back/seat/service/SeatService.java
@@ -4,17 +4,18 @@ import com.example.back.seat.dto.SeatDto;
 import com.example.back.seat.dto.SeatMapper;
 import com.example.back.seat.repository.SeatRepository;
 import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.misc.Pair;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class SeatService {
     private final SeatRepository seatRepository;
     private final SeatMapper seatMapper;
+    Map<Integer, List<Integer>> sumsAndCombinationsConsecutive = new HashMap<>();
+    Map<Integer, List<Integer>> sumsAndCombinationsNotConsecutive = new HashMap<>();
     public List<SeatDto> getSeatsByMovieId(Long movieId) {
         List<SeatDto> allSeats = seatMapper.toDtoList(seatRepository.findAll());
         List<SeatDto> seatsByMovie = new ArrayList<>();
@@ -27,60 +28,128 @@ public class SeatService {
     }
 
     public List<Integer> getRecommendedSeats(Long movieId, Integer capacity) {
+        List<Integer> bestCombination;
         List<SeatDto> seats = getSeatsByMovieId(movieId);
-//        List<Integer> recommendedSeats = new ArrayList<>();
-//        int totalSeats = seats.size();
-//
-//        for (int i = 0; i < totalSeats - capacity + 1; i++) {
-//            if (!seats.get(i).isBooked()) {
-//                boolean validRecommendation = true;
-//
-//                for (int j = i; j < i + capacity; j++) {
-//                    if (seats.get(j).isBooked()) {
-//                        validRecommendation = false;
-//                        break;
-//                    }
-//                }
-//
-//                if (validRecommendation) {
-//                    for (int j = i; j < i + capacity; j++) {
-//                        recommendedSeats.add(j + 1); // Adding seat number (1-indexed)
-//                    }
-//                    break; // Recommend the first valid set of seats found
-//                }
-//            }
-//        }
-//        System.out.println(recommendedSeats);
-//        return recommendedSeats;
-        List<Integer> bestRecommendation = new ArrayList<>();
-        int bestDistance = Integer.MAX_VALUE;
-
-        int numRows = 7; // Assuming 6 rows
-        int numCols = 10; // Assuming 10 columns
-
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col <= numCols - capacity; col++) {
-                int startIndex = row * numCols + col;
-                if (!seats.get(startIndex).isBooked()) {
-                    int distance = calculateDistance(row, col, capacity);
-                    if (distance < bestDistance) {
-                        bestDistance = distance;
-                        bestRecommendation = new ArrayList<>();
-                        for (int j = 0; j < capacity; j++) {
-                            bestRecommendation.add(startIndex + j);
-                        }
-                    }
-                }
+        List<SeatDto> available = new ArrayList<>();
+        for (SeatDto seatDto : seats) {
+            if (!seatDto.isBooked()) {
+                available.add(seatDto);
             }
         }
 
-        return bestRecommendation;
+        List<List<Integer>> consecutiveSeatsAvailable = findConsecutiveSeatRanges(available, capacity);
+        System.out.println(consecutiveSeatsAvailable);
+        List<List<Integer>> consecutiveSeats = findConsecutiveSeatRanges(seats, capacity);
+
+        float middle = getMiddleValue(capacity, consecutiveSeats);
+
+        getSumsForCombinations(consecutiveSeatsAvailable);
+
+        if (!sumsAndCombinationsConsecutive.isEmpty()) {
+            bestCombination = getBestCombination(sumsAndCombinationsConsecutive, middle);
+        } else {
+            bestCombination = getBestCombination(sumsAndCombinationsNotConsecutive, middle);
+        }
+
+
+        System.out.println(bestCombination);
+        return bestCombination;
     }
-    private static int calculateDistance(int row, int col, int capacity) {
-        int middleRow = 4; // Assuming 6 rows, middle row is 3
-        int middleCol = col + capacity / 2;
-        int rowDistance = Math.abs(middleRow - row);
-        int colDistance = Math.abs(middleCol - col);
-        return rowDistance + colDistance;
+
+
+    public static List<List<Integer>> findConsecutiveSeatRanges(List<SeatDto> seatList, int ticketsRequested) {
+        List<List<Integer>> consecutiveSeatRanges = new ArrayList<>();
+
+        for (int i = 0; i < seatList.size(); i++) {
+            SeatDto currentSeat = seatList.get(i);
+            Integer seatColumn = getColumn(currentSeat);
+
+//            if (seatColumn + ticketsRequested - 1 <= 70) {
+                List<Integer> currentRange = new ArrayList<>();
+
+                for (int j = i; j < i + ticketsRequested; j++) {
+                    if (j < seatList.size()) {
+                        SeatDto seat = seatList.get(j);
+                        currentRange.add(seat.seatNumber());
+                    }
+                }
+                if (currentRange.size() == ticketsRequested) {
+                    consecutiveSeatRanges.add(currentRange);
+                }
+//            }
+        }
+        return consecutiveSeatRanges;
+    }
+
+    private float getMiddleValue(int capacity, List<List<Integer>> allCombinations) {
+        Integer listLength = allCombinations.size();
+        if (listLength % 2 != 0) {
+            listLength -= 1;
+        }
+        List<Integer> middleList = allCombinations.get(listLength / 2);
+        Integer sum = middleList.stream().mapToInt(Integer::intValue).sum();
+        return sum;
+
+    }
+
+    private void getSumsForCombinations(List<List<Integer>> availableCombinations) {
+        for (List<Integer> combination : availableCombinations) {
+            int sum = combination.stream().mapToInt(Integer::intValue).sum();
+            boolean consecutive = isConsecutive(combination);
+            if (consecutive) {
+                sumsAndCombinationsConsecutive.computeIfAbsent(sum, k -> combination);
+            } else {
+                sumsAndCombinationsNotConsecutive.computeIfAbsent(sum, k -> combination);
+            }
+        }
+    }
+
+    private List<Integer> getBestCombination(Map<Integer, List<Integer>> sumsAndCombinations, float middle) {
+        List<Integer> bestMatch = new ArrayList<>();
+
+        float smallestRange = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, List<Integer>> entry: sumsAndCombinations.entrySet()) {
+            Integer sum = entry.getKey();
+            List<Integer> combination = entry.getValue();
+
+            if (Math.abs(middle - sum) < smallestRange) {
+                smallestRange = Math.abs(middle - sum);
+                bestMatch = combination;
+            }
+
+
+        }
+
+        return bestMatch;
+    }
+    private boolean isConsecutive(List<Integer> toCheck) {
+        for (int i = 1; i < toCheck.size(); i++) {
+            if (toCheck.get(i) != toCheck.get(i - 1) + 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    private static Integer getRow(SeatDto seatDto) {
+        Integer seatNumber = seatDto.seatNumber();
+        Integer row = 0;
+        if (seatNumber < 10) {
+            row = 1;
+        } else {
+            row = seatNumber / 10;
+        }
+        return row;
+    }
+
+    private static Integer getColumn(SeatDto seatDto) {
+        Integer seatNumber = seatDto.seatNumber();
+        Integer column = 0;
+        if (seatNumber < 10) {
+            column = seatNumber;
+        } else {
+            column = seatNumber % 10;
+        }
+        return column;
     }
 }
